b9380abc9fe3f2ef3450c961529be30ac7b10a07
AlejandroIMP <alejandrosunuc@gmail.com>
2025-09-20 11:01:17 -0600

    feat: Database connection and migrations

---
+++ ProductManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Models.Entities;
+
+namespace ProductManagement.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
--- ProductManagement/Models/Entities/Product.cs
+++ ProductManagement/Models/Entities/Product.cs
@@ -2,5 +2,12 @@
 
 public class Product
 {
-    
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required decimal Price { get; set; }
+    public required int Stock { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool? IsActive { get; set; }
 }
