3bcea9dc6e614d3595e72bf9c1d30e954f9c2479
AlejandroIMP <alejandrosunuc@gmail.com>
2025-09-21 16:28:54 -0600

    feat: implement AddProductDto and AddProduct endpoint in ProductController

--- ProductManagement/Controllers/ProductController.cs
+++ ProductManagement/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductManagement.Data;
+using ProductManagement.Models;
+using ProductManagement.Models.Entities;
 
 namespace ProductManagement.Controllers
 {
@@ -20,6 +22,45 @@ namespace ProductManagement.Controllers
         {
             var allProducts = _dbContext.Products.ToList();
             return Ok(allProducts);
+        }
+        
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetProductById(Guid id)
+        {
+            var product = _dbContext.Products.Find(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(AddProductDto addProductDto)
+        {
+            var productEntity = new Product()
+            {
+                Name = addProductDto.Name,
+                Description = addProductDto.Description,
+                Price = addProductDto.Price,
+                Stock = addProductDto.Stock,
+                IsActive = addProductDto.IsActive,
+            };
+
+            try
+            {
+                _dbContext.Products.Add(productEntity);
+                _dbContext.SaveChanges();
+                return Ok(productEntity);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
---
+++ ProductManagement/Models/AddProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManagement.Models;
+
+public class AddProductDto
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required decimal Price { get; set; }
+    public required int Stock { get; set; }
+    public bool? IsActive { get; set; }
+}
